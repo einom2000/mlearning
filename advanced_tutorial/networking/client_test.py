3be06aaebf7d71f57ed3046fced2fdbb8a5d3651
einom.ng@gmail.com <starBB002347>
2019-08-30 18:07:44 +0800

    qq

--- advanced_tutorial/networking/client_test.py
+++ advanced_tutorial/networking/client_test.py
@@ -1,23 +1,31 @@
 import socket
 import sys
+import datetime
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('144.202.93.164', 15200)  #
 print('connecting to %s port %s' % server_address, file=sys.stderr)
 sock.connect(server_address)
+
+
 try:
-    message = 'This is a test message. It will be repeated.'
-    print('sending %s' % message, file=sys.stderr)
-    sock.sendall(message.encode('utf-8') )
+    for _ in range(10):
+        message = str(datetime.datetime.now())
+        print('sending %s' % message, file=sys.stderr)
+        sock.sendall(message.encode('utf-8'))
 
-    amount_received = 0
-    amount_expected = len(message)
+        amount_received = 0
+        amount_expected = len(message)
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received = len(data)
-        print('received: %s' % data, file=sys.stderr)
+        while amount_received < amount_expected:
+            data = sock.recv(128)
+            amount_received = len(data)
+            print('received: %s' % data, file=sys.stderr)
+        time.sleep(1)
 finally:
+    message = 'ENDED!'
+    sock.sendall(message.encode('utf-8'))
     print('colosing socket', file=sys.stderr)
     sock.close()
---
+++ s_c_link/test.py
@@ -0,0 +1,5 @@
+import sys
+import socket
+
+data = 'kkkk'
+print('111111 + %s' % data, file=sys.stderr)
