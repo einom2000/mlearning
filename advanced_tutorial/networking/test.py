9cf2c16b3139cc1e8665325e9667ac751c6c17f9
einom2000 <einom.ng@gmail.com>
2019-09-02 16:09:33 +0800

    111

--- advanced_tutorial/networking/test.py
+++ advanced_tutorial/networking/test.py
@@ -6,10 +6,3 @@ text = '{"bot_hears": "pause", "pet_train": "pause"}'
 
 client_sending.sending(text, False)
 
-with open('test.json', 'w') as f:
-    f.write(text)
-
-with open('test.json') as f:
-    dict = json.load(f)
-
-print(dict)
