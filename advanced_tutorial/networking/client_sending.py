77d3257411026541c178e2f1a792f02ba278230e
einom2000 <einom.ng@gmail.com>
2019-09-02 15:59:36 +0800

    111

--- advanced_tutorial/networking/client_sending.py
+++ advanced_tutorial/networking/client_sending.py
@@ -1,35 +1,43 @@
+
 import communication
 import json
 import rsa_encrypto
 
-en_file= 'bin\\en.bin'
-key_file = 'bin\\keys_here.json'
 
-with open(key_file) as f:
-    keys = json.load(f)
+def sending(text, show_detail):
+    en_file = 'bin\\en.bin'
+    key_file = 'bin\\keys_here.json'
+    server_file = 'bin\\server_ip.json'
+    with open(key_file) as f:
+        keys = json.load(f)
 
-recipient_pub = keys['SERVER_PUB']
+    recipient_pub = keys['SERVER_PUB']
 
-TEXT = 'are you ready'
+    rsa_encrypto.encryption(text, recipient_pub, en_file)
 
-rsa_encrypto.encryption(TEXT, recipient_pub,en_file)
+    with open(en_file, 'rb') as f:
+        data = f.read()
 
-with open(en_file, 'rb') as f:
-    data = f.read()
+    with open(server_file) as f:
+        server = json.load(f)
 
-with open('bin\\server_ip.json') as f:
-    server = json.load(f)
+    server_address = (server['server_ip'], server['server_port'])
 
-server_address = (server['server_ip'], server['server_port'])
+    sock = communication.sock()
 
-sock = communication.sock()
+    communication.connect(sock, server_address)
 
-communication.connect(sock, server_address)
+    communication.send(sock, b'HELLO', show_detail)
+    communication.check_responce(sock, b'OK', 128)
+    communication.send(sock, data, show_detail)
+    communication.close(sock)
 
-communication.send(sock, b'HELLO', False)
-communication.check_responce(sock, b'OK', 128)
-communication.send(sock, data, False)
-communication.close(sock)
 
+def main():
+    pass
+
+
+if __name__ == '__main()__':
+    main()
 
 
---
+++ advanced_tutorial/networking/test.py
@@ -0,0 +1,9 @@
+import client_sending
+
+text = 'Nov 7, 2018 - Using the -exec flag, files can be found and immediately processed within the same command. ' + \
+       'Find Linux Files by Name or Extension. Common Linux Find Commands and Syntax. Options and Optimization for' + \
+       'Find. Find Files by Modification Time. Use Grep to Find Files Based on Content.'
+
+
+
+client_sending.sending(text, False)
