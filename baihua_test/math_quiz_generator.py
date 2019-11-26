b511ad437e5ac9fd1be0dadb6ae5db28de884b58
einom2000 <einom.ng@gmail.com>
2019-11-26 15:29:15 +0800

    111

--- baihua_test/math_quiz_generator.py
+++ baihua_test/math_quiz_generator.py
@@ -26,22 +26,39 @@ def quiz_create(field, obj, calc, quiz_number):
                 'subtraction': '-',
                 'multiplication': 'x',
                 'division': '÷'}
-    if field[1] - field[0] <= quiz_number:
-        field[1] = quiz_number + field[0]
-    subjs = random.sample(range(field[0], field[1] + 1), quiz_number)
+    if field[1] - field[0] + 1 < quiz_number:
+        length = field[1] - field[0] + 1
+    else:
+        length = quiz_number
+    length = length // 2 * 2
+    subjs = random.sample(range(field[0], field[1] + 1), length)
     quizs = []
     for subj in subjs:
         tmp = [subj, random.choice(obj)]
         random.shuffle(tmp)
         quizs.append(tmp)
     quiz_table = []
+    quiz_table1 = []
     for i in range(0, len(quizs), 2):
         quiz_table.append((str(quizs[i][0]) + ' ' + calc_dic[calc] + ' ' + str(quizs[i][1]) + ' =',
                            str(quizs[i + 1][0]) + ' ' + calc_dic[calc] + ' ' + str(quizs[i + 1][1]) + ' ='))
-    return quiz_table
+    if quiz_number > length:
+        length1 = quiz_number - length
+        if length1 + length > max_quizs_per_page:
+            length1 = max_quizs_per_page - length
+        subjs = random.sample(range(field[0], field[1] + 1), length1)
+        quizs = []
+        for subj in subjs:
+            tmp = [subj, random.choice(obj)]
+            random.shuffle(tmp)
+            quizs.append(tmp)
+        for i in range(0, len(quizs), 2):
+            quiz_table1.append((str(quizs[i][0]) + ' ' + calc_dic[calc] + ' ' + str(quizs[i][1]) + ' =',
+                               str(quizs[i + 1][0]) + ' ' + calc_dic[calc] + ' ' + str(quizs[i + 1][1]) + ' ='))
+    return quiz_table + quiz_table1
 
 
-def create_doc(quiz_table, file_number):
+def create_doc(i):
     document = Document()
     number = obj[0]  # Subtraction multiplication division
     title1 = datetime.now().date().strftime('%Y/%m/%d') + ' -- '
@@ -50,65 +67,68 @@ def create_doc(quiz_table, file_number):
     title4 = str(number) + '\n'
     title5 = trans(calc)
 
-    # title creation
-    p = document.add_heading(level=0)
-    wp = p.add_run(title1)
-    wp.font.size = Pt(25)
-    wp.font.bold = True
-    wp.font.color.rgb = RGBColor(0, 0, 0)
-    wp = p.add_run(title2)
-    wp.font.size = Pt(30)
-    wp.font.color.rgb = RGBColor(255, 0, 0)
-    wp = p.add_run(title3)
-    wp.font.size = Pt(25)
-    wp.font.bold = True
-    wp.font.color.rgb = RGBColor(0, 0, 0)
-    wp = p.add_run(title4)
-    wp.font.size = Pt(30)
-    wp.font.color.rgb = RGBColor(255, 0, 0)
+    for page in range(i):
+        # title creation
+        p = document.add_heading(level=0)
+        wp = p.add_run(title1)
+        wp.font.size = Pt(25)
+        wp.font.bold = True
+        wp.font.color.rgb = RGBColor(0, 0, 0)
+        wp = p.add_run(title2)
+        wp.font.size = Pt(30)
+        wp.font.color.rgb = RGBColor(255, 0, 0)
+        wp = p.add_run(title3)
+        wp.font.size = Pt(25)
+        wp.font.bold = True
+        wp.font.color.rgb = RGBColor(0, 0, 0)
+        wp = p.add_run(title4)
+        wp.font.size = Pt(30)
+        wp.font.color.rgb = RGBColor(255, 0, 0)
 
-    wp = p.add_run('关于数字 ')
-    wp.font.size = Pt(25)
-    wp.font.bold = True
-    wp.font.color.rgb = RGBColor(0, 0, 0)
-    wp = p.add_run(str(number))
-    wp.font.size = Pt(30)
-    wp.font.color.rgb = RGBColor(255, 0, 0)
-    wp = p.add_run(' 的')
-    wp.font.size = Pt(25)
-    wp.font.bold = True
-    wp.font.color.rgb = RGBColor(0, 0, 0)
-    wp = p.add_run(title5)
-    wp.font.size = Pt(30)
-    wp.font.color.rgb = RGBColor(255, 0, 0)
-    wp = p.add_run('练习')
-    wp.font.size = Pt(25)
-    wp.font.color.rgb = RGBColor(0, 0, 0)
-
-    table = document.add_table(rows=1, cols=2)
-
-    for q1, q2 in quiz_table:
-        row_cells = table.add_row().cells
-        row_cells[0].text = q1
-        row_cells[1].text = q2
-
-    for row in table.rows:
-        for cell in row.cells:
-            paragraphs = cell.paragraphs
-            for paragraph in paragraphs:
-                for run in paragraph.runs:
-                    font = run.font
-                    font.size = Pt(30)
-
-    document.save('math_quiz' + '0' * (3 - len(str(file_number))) + str(file_number) + '.docx')
+        wp = p.add_run('关于数字 ')
+        wp.font.size = Pt(25)
+        wp.font.bold = True
+        wp.font.color.rgb = RGBColor(0, 0, 0)
+        wp = p.add_run(str(number))
+        wp.font.size = Pt(30)
+        wp.font.color.rgb = RGBColor(255, 0, 0)
+        wp = p.add_run(' 的')
+        wp.font.size = Pt(25)
+        wp.font.bold = True
+        wp.font.color.rgb = RGBColor(0, 0, 0)
+        wp = p.add_run(title5)
+        wp.font.size = Pt(30)
+        wp.font.color.rgb = RGBColor(255, 0, 0)
+        wp = p.add_run('练习 -- 第 ' + str(page + 1) + ' 页')
+        wp.font.size = Pt(25)
+        wp.font.color.rgb = RGBColor(0, 0, 0)
 
 
-obj = [2,]
-quiz_number = 10
-field = [0, 12]
+        quiz_table = quiz_create(field, obj, calc, quiz_number)
+        table = document.add_table(rows=1, cols=2)
+
+        for q1, q2 in quiz_table:
+            row_cells = table.add_row().cells
+            row_cells[0].text = q1
+            row_cells[1].text = q2
+
+        for row in table.rows:
+            for cell in row.cells:
+                paragraphs = cell.paragraphs
+                for paragraph in paragraphs:
+                    for run in paragraph.runs:
+                        font = run.font
+                        font.size = Pt(30)
+        if page < i -1:
+            document.add_page_break()
+
+    document.save('math_quiz' + '_' + datetime.now().date().strftime('%Y_%m_%d') + '.docx')
+
+
+obj = [2,]          # 2 to plus the other number
+quiz_number = 16    # quiz per page in 2 columns
+field = [0, 12]     # 0 ~ 12 number to plus
 calc = 'addition'
+max_quizs_per_page = 20
 
-
-for i in range(0, 3):
-    quiz_table = quiz_create(field, obj, calc, quiz_number)
-    create_doc(quiz_table, i)
+create_doc(3)
