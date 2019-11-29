43c21707b8b8968929a2b4695ec25b63834c234f
einom2000 <einom.ng@gmail.com>
2019-11-29 11:01:25 +0800

    111

--- baihua_test/size_squence.py
+++ baihua_test/size_squence.py
@@ -3,6 +3,21 @@ import random
 import keyboard
 import sys
 from datetime import datetime
+from PIL import Image
+
+
+def draw_circle(x, y, radius, color):
+    t = turtle.Turtle()
+    t.speed(0)
+    t.hideturtle()
+    t.penup()
+    t.color(color)
+    t.setx(x)
+    t.sety(y - radius)
+    t.pendown()
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
 
 
 def draw_grid():
@@ -24,31 +39,20 @@ def draw_grid():
 
     position = []
     for i in range(2):
-        y = grid_height // 2 - grid_height // 4 - i * grid_height // 2 - trimy
+        y = grid_height // 2 - grid_height // 4 - i * grid_height // 2
         for j in range(4):
-            x = grid_width // 8 + j * grid_width // 4 - grid_width // 2 - trimx
+            x = grid_width // 8 + j * grid_width // 4 - grid_width // 2
             position.append((x, y))
 
     return position
 
 
 def draw_sizes(position):
-    t = turtle.Turtle()
-    t.speed(0)
-    t.hideturtle()
-    t.penup()
-    t.color('red')
     random.shuffle(sizes)
 
     for pos in position:
-        t.setx(pos[0])
-        t.sety(pos[1])
-        t.pendown()
-        size = sizes[position.index(pos)]
-        color = colors[position.index(pos)]
-        t.color(color)
-        t.write("★", font=("Arial", size, "normal"))  # ★
-        t.penup()
+        print(pos)
+        draw_circle(pos[0], pos[1], sizes[position.index(pos)], colors[position.index(pos)])
 
 
 def write_info(text):
@@ -71,9 +75,7 @@ folder = 'e:\\einom\\Documents\\___SOPHIA____\\--Sophia K2 Folder\\BAIHUA_LOGICS
 time_to_remember = 30
 canvas_width = 1000
 canvas_height = 600
-trimx = 80
-trimy = 100
-sizes = [35, 50, 65, 80, 95, 110, 125, 140]
+sizes = [10 + 14 * x for x in range(8)]
 colors = ['red', 'green', 'blue', 'brown', 'black', 'gray', 'pink', 'orange']
 style = ('Courier', 10, 'bold')
 
@@ -83,22 +85,30 @@ wn.title("color_matrix")
 wn.setup(canvas_width, canvas_height)
 grid_width = canvas_width - 100
 grid_height = canvas_height - 100
+circles = []
 
-
-
+k = 1
 while True:
 
     position = draw_grid()
     draw_sizes(position)
 
-    write_info('pleas press \'space\' to new one!')
+    write_info('★★★pleas press \'space\' to new one!★★★')
 
     while True:
 
         if keyboard.is_pressed('space'):
+            turtle.getscreen().getcanvas().postscript(file='tmp.ps', colormode='color')
+
+            img = Image.open('tmp.ps')
+            try:
+                img.save(folder + 'size_squence_' + today + '0' * (3 - len(str(k))) + str(k) + '.jpg')
+            except FileNotFoundError:
+                pass
             break
         if keyboard.is_pressed('q'):
             sys.exit()
 
     wn.clear()
 
+    k += 1
