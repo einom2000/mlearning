6b873afe7f7fee706e02248c4d1b7186f911e477
einom <einom.ng@Gmail.com>
2020-12-06 19:04:42 +0800

    11

--- encryp.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def main():
-
-if __name__ == '__main__':
-    main()
---
+++ tensorflow2/Deep Computer Vision.py
@@ -0,0 +1,20 @@
+# https://colab.research.google.com/drive/1ZZXnCjFEOkp_KdNcNabd14yok0BAIuwS#forceEdit=true&sandboxMode=true
+
+#Imports
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+keras = tf.keras
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+# split the data manually into 80% training, 10% testing, 10% validation
+(raw_train, raw_validation, raw_test), metadata = tfds.load(
+    'cats_vs_dogs',
+    split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+    with_info=True,
+    as_supervised=True,
+)
+
