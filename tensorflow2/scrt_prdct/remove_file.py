fe00441b8373971c00df69075ad3865bd65b09f3
einom <einom.ng@Gmail.com>
2021-01-14 12:12:22 +0800

    Delete tensorflow2/scrt_prdct directory

--- tensorflow2/scrt_prdct/remove_file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def remove(targetdir, endswith="@", startwith="@"):
-    for fname in os.listdir(targetdir):
-        if fname.endswith(endswith):
-            os.remove(os.path.join(targetdir, fname))
-        elif fname.startswith(startwith):
-            os.remove(os.path.join(targetdir, fname))
--- tensorflow2/scrt_prdct/tool_collect_csv.py
+++
@@ -1,6 +0,0 @@
-import day_to_csv
-
-# day_to_csv.day_to_csv(single_code=600585, market='sh')
-# day_to_csv.day_to_csv(market='sh')
-
-day_to_csv.day_to_csv(market='sz')
